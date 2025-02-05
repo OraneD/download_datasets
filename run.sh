520b77ef664c2198f2a4eb087bea693d1eb1461a
Orane Dufour <ordufour@oranepc.loria.fr>
2025-02-05 12:18:05 +0100

    launch slurm job

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=ExtractTar
+#SBATCH --output=ExtractTar%j.out
+#SBATCH --error=ExtractTar%j.err
+#SBATCH --ntasks=30
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=10
+#SBATCH --time=05:00:00
+#SBATCH --account=xyz@v100# nom du job
+
+module purge
+conda deactivate
+module load pytorch-gpu/py3/2.3.0
+set -x
+srun python extract_tar.py $SCRATCH/LibriHeavy/heavy_trials_.tar.gz $SCRATCH/LibriHeavy/
