373fe6ddc6c4bda576a960c598104e0924839091
OraneD <orane.dufour@gmail.com>
2025-02-05 17:10:48 +0100

    sbatch Vox1

---
+++ prepare_voxceleb1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=PrepareVox1
+#SBATCH --output=PrepareVox1%j.out
+#SBATCH --error=PrepareVox1%j.err
+#SBATCH --ntasks=30
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=10
+#SBATCH --time=05:00:00
+#SBATCH --account=yjs@v100
+
+source ~/.bashrc
+conda activate kiwano_env
+set -x
+python ../kiwano/recipes/resnet/local/prepare_voxceleb1.py --vad --num_jobs 30 $SCRATCH/voxceleb2/ $SCRATCH/resnet_data/voxceleb1/
