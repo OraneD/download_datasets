39c04421a28b2a5f21bb2f5756bc47d60cd5178c
OraneD <orane.dufour@gmail.com>
2025-02-05 17:17:42 +0100

    sbatch Libri B5

---
+++ extract_Libri_anon.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=ExtractTar
+#SBATCH --output=ExtractTar%j.out
+#SBATCH --error=ExtractTar%j.err
+#SBATCH --ntasks=30
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=10
+#SBATCH --time=15:00:00
+#SBATCH --account=yjs@v100
+
+source ~/.bashrc
+conda activate kiwano_env
+set -x
+srun python extract_tar.py $SCRATCH/LibriHeavy/heavy_trials_anon_B5.tar.gz $SCRATCH/LibriHeavy/
